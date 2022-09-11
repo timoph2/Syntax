464504fddf191b66d45ac9094908190a7414f93c
timoph2 <98256868+timoph2@users.noreply.github.com>
2022-09-11 20:35:10 +0800

    Add files via upload

---
+++ Javascript  11.js
@@ -0,0 +1,12 @@
+
+function multiplication_table(n) {
+let multiplier = 1
+
+    while (multiplier < 11){
+    let result = multiplier * n
+        console.log(`${multiplier} x ${n} = ${result}`)
+        multiplier += 1
+    }}
+
+multiplication_table(7)
+
