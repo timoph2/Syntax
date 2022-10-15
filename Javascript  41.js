495f0e89b80813663351d7198902dbebcdd025b6
timoph2 <98256868+timoph2@users.noreply.github.com>
2022-10-15 14:35:01 +0800

    Add files via upload

---
+++ Javascript  41.js
@@ -0,0 +1,37 @@
+
+
+function printNumberInWord(num){
+    
+    switch (num){
+        case 1:
+            console.log("One") 
+            break;
+        case 2:
+            console.log("Two") 
+            break;
+        case 3:
+            console.log("Three") 
+            break;
+        case 4:
+            console.log("Four") 
+            break;
+        default:
+            console.log('other')
+        }}
+
+ 
+
+printNumberInWord(2)
+printNumberInWord(4)
+printNumberInWord(55)
+
+
+
+
+
+
+
+
+
+
+
