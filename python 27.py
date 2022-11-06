567bdd1669a266d94acb6663d8014e4a34dcb662
timoph2 <98256868+timoph2@users.noreply.github.com>
2022-11-07 00:47:29 +0800

    Add files via upload

---
+++ python 27.py
@@ -0,0 +1,23 @@
+ 
+
+
+def shouldWakeUp(barking,hourOfday):
+    if barking or hourOfday < 8 or hourOfday > 22 :
+        return True
+    else:
+        return False
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
