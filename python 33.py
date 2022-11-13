8d1eae62a9d15321209b49eec3d8db512ca6acd7
timoph2 <98256868+timoph2@users.noreply.github.com>
2022-11-13 14:17:15 +0800

    Add files via upload

---
+++ python 33.py
@@ -0,0 +1,30 @@
+
+
+def printNumberInWord(num):
+    
+    match num:
+        case 1:
+            print("One") 
+        case 2:
+            print("Two") 
+        case 3:
+            print("Three") 
+        case 4:
+            print("Four") 
+        case _:
+            print('other')
+    
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
